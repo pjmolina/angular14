53044d1b734813c02b96bbe0ca9a3505367c430c
Pedro J. Molina <pjmolina@gmail.com>
2023-06-15 19:31:01 +0200

    *ngFor

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user-profile/user-profile.component';
 
+interface Persona {
+  firstName: string;
+  lastName: string;
+  role: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +14,24 @@ import { User } from '../user-profile/user-profile.component';
 })
 export class AppComponent implements OnInit {
   title = 'mi aplicacion 23';
+
+  personas: Persona[] = [
+    {
+      firstName: 'Jesica',
+      lastName: 'Alba',
+      role: 'Admin',
+    },
+    {
+      firstName: 'Pepe',
+      lastName: 'Lopez',
+      role: 'Operador',
+    },
+    {
+      firstName: 'Maria',
+      lastName: 'Zamora',
+      role: 'Admin',
+    },
+  ];
 
   constructor() {
     console.log('1. appcomponent constructor');

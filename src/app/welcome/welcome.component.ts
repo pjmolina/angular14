78816175b2b59dd25fd0c2d1e3b4cc5377ea73ed
Pedro J. Molina <pjmolina@gmail.com>
2023-06-28 18:02:52 +0200

    ejemplo de compos enlazados

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent {
+  provincias = ['Sevilla', 'Cordoba', 'Huelva', 'Malaga', 'Cadiz'];
+  municipios = [
+    {
+      name: 'Sevilla',
+      provincia: 'Sevilla'
+    },
+    {
+      name: 'Bormujos',
+      provincia: 'Sevilla'
+    },
+    {
+      name: 'San Roque',
+      provincia: 'Cadiz'
+    },
+    {
+      name: 'Jerez',
+      provincia: 'Cadiz'
+    }
+  ];
 
+  estado = {
+    provinciaSeleccionada: null,
+    poblacionSeleccionada: null
+  };
+
+  get poblaciones(): string[] {
+    const prov = this.estado.provinciaSeleccionada;
+    if (!prov) {
+      return [];
+    }
+    return this.municipios
+      .filter((m) => m.provincia === prov)
+      .map((m) => m.name);
+  }
 }
